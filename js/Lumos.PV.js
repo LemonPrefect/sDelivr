086c20d0abbce7756a8bfabc2aa84a5a297dd328
LemonPrefect <jingzhuokwok@gmail.com>
2020-03-05 16:10:30 +0800

    Mod:Lumos.PV.js

--- js/Lumos.PV.js
+++ js/Lumos.PV.js
@@ -5,10 +5,8 @@ details["PASSAGE_TITLE"] = document.title;
 details["IP_ADDRESS"] = returnCitySN["cip"];
 details["LOCATION"] = returnCitySN["cname"];
 data = JSON.stringify(details);
-console.log(data);
 data = BASE64.encode(data);
-console.log(data);
-data = "23RY/" + data;
+data = "RY23/" + data;
 
 
 $.ajax({
