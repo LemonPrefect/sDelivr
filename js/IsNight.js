40373cb243f543920eb8472f97128c2559509976
LemonPrefect <jingzhuokwok@gmail.com>
2020-03-11 11:39:21 +0800

    Prove:IsNight.js

--- js/IsNight.js
+++ js/IsNight.js
@@ -30,13 +30,10 @@ if($.cookie('sunrise') === undefined || $.cookie('sunset') === undefined || retu
 }
 let timeNowCompare = parseInt(timestampNow / 1000);
 if(timeNowCompare  > $.cookie('sunset') || timeNowCompare < $.cookie('sunrise')){
-    //var shade = document.createElement('div');
-    //shade.innerHTML = '<div style="position:fixed;background-color:#000;top:0;left:0;z-index:2147483647;pointer-events:none;opacity:0.33;width:100%;height:100%"></div>';
-    //document.body.appendChild(shade);
     console.log("Good Evening!");
+    console.log("Enable the force dark mode on browser might help to get better experience.");
 }else{
-    console.log("Maybe it's daytime now.")
+    console.log("Maybe it's daytime now.");
 }
-console.log("日出：" + $.cookie('sunrise'));
-console.log("日落：" + $.cookie('sunset'));
-console.log("咱不会写不用Pjax的局部刷新夜间模式~");
+console.log("Timestamp Sunrise:" + $.cookie('sunrise'));
+console.log("Timestamp Sunset:" + $.cookie('sunset'));
